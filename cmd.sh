a4c9e1bf0cdc7c232ed5f52f9177aeba7cf7a13d
Herson Lozano <44881576+hersonxlg@users.noreply.github.com>
2024-05-18 23:24:28 -0600

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,6 @@
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd ~/.vim/colors
+curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
