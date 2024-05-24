647461f9499d694270c60b68d97a97f2efc1d1b8
Herson Lozano <44881576+hersonxlg@users.noreply.github.com>
2024-05-24 02:46:19 -0600

    Update INSTALL.sh

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,10 @@
+# Descargar el administrador de paquetes "autoload"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 cd ~/.vim/colors
 curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 
+
+mv .vimrc ~/
