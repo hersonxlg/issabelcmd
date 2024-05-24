392d0622c0aa84ef2a7c7cf260e996d5880b61a2
Herson Lozano <44881576+hersonxlg@users.noreply.github.com>
2024-05-24 03:00:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
+temp=$(pwd)
+
+mkdir ~/.vim
+mkdir ~/.vim/autoload
+mkdir ~/.vim/backup
+mkdir ~/.vim/colors
+mkdir ~/.vim/plugged
+
 # Descargar el administrador de paquetes "autoload"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
+# Descargar el esquema de colores "molokai" para vim.
 cd ~/.vim/colors
 curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 
-
+cd $tem
 mv .vimrc ~/
