64d3ede2c615b401ae4b9d8c4ad86dd163e8fef1
Herson Lozano <44881576+hersonxlg@users.noreply.github.com>
2024-05-24 08:56:50 -0600

    Update install_googletts.sh

--- install_googletts.sh
+++ install_googletts.sh
@@ -1,6 +1,6 @@
 cd ~/
 yum update
 yum install perl,perl-libwww,perl-LWP-Protocol-https,sox,mpg123,format_sln -y
-git clone git://github.com/zaf/asterisk-googletts
+git clone https://github.com/zaf/asterisk-googletts.git
 cd ~/asterisk-googletts
 cp googletts.agi /var/lib/asterisk/agi-bin/
