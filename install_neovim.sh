61bc26277fe5bb29b7164d5ab2461e768590e157
Herson Lozano <44881576+hersonxlg@users.noreply.github.com>
2024-05-24 19:51:15 -0600

    Update install_neovim.sh

--- install_neovim.sh
+++ install_neovim.sh
@@ -4,3 +4,5 @@ yum -y install epel-release
 curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
 yum -y install neovim
 cd $temp
+mkdir ~/.config/nvim
+cp -r ./neovim/* ~/.config/nvim
